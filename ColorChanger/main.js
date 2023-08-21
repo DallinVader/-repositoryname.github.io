5ef272fcc5fbb0d99e3bb55be07eb52d3eb2ea26
DallinVader <dallin@riafox.com>
2023-08-21 13:42:20 -0600

    commit

--- ColorChanger/main.js
+++ ColorChanger/main.js
@@ -1,4 +1,13 @@
-function ChangePageToCollorChanger(){
-    console.log("Change Page");
-    location.href = "ColorChanger";
+var CurrentColor;
+var StyleSheetForJS = document.body;
+
+function ChangeColorOnColorPage() {
+    // Generate a random color code in hexadecimal format
+    CurrentColor = "#" + (Math.random() * 16777215 | 0).toString(16);
+    console.log(CurrentColor);
+
+    // Set the background color of the body
+    StyleSheetForJS.style.backgroundColor = CurrentColor;
+
+    return CurrentColor;
 }
