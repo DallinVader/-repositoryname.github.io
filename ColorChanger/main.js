1f1de85450dd46d2c17477588259ccfa7c45941e
DallinVader <dallin@riafox.com>
2023-08-21 15:09:17 -0600

    Added Image that disapears when speed value changes and added speed value

--- ColorChanger/main.js
+++ ColorChanger/main.js
@@ -1,13 +1,14 @@
 var CurrentColor;
 var StyleSheetForJS = document.body;
 var Speed = 1000;
-var intervalId = clicked;
+var intervalId;
 
 function OnStart(){
     
 }
 
 function RandomSliderFunc(){
+    StyleSheetForJS.style.backgroundImage = "none";
     CurrentColor = "#" + (Math.random() * 16777215 | 0).toString(16);
     StyleSheetForJS.style.backgroundColor = CurrentColor;
 
