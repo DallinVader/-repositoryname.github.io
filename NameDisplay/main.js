f1a9219951b13f8166232f5b85aa02c2f0cf412e
DallinVader <dallin@riafox.com>
2023-08-21 16:29:18 -0600

    Fixed Hello There Text

--- NameDisplay/main.js
+++ NameDisplay/main.js
@@ -1,6 +1,8 @@
 function UpdateName(){
     var NameInput = document.getElementById("NameInput")
     var NameOutText = document.getElementById("InName")
+    var HelloThere = document.getElementById("HelloThere")
+    HelloThere.innerText = "Hello There"
     NameOutText.innerText = NameInput.value
     console.log(NameInput.value);
 }
