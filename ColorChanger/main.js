1729e18d765c4064cf5d9fc787c59f60fe682740
DallinVader <dallin@riafox.com>
2023-08-21 14:36:22 -0600

    Added Slider for color change speed

--- ColorChanger/main.js
+++ ColorChanger/main.js
@@ -1,13 +1,19 @@
 var CurrentColor;
 var StyleSheetForJS = document.body;
+var Speed = 1000;
+var intervalId = clicked;
 
-function ChangeColorOnColorPage() {
-    // Generate a random color code in hexadecimal format
+function OnStart(){
+    
+}
+
+function RandomSliderFunc(){
     CurrentColor = "#" + (Math.random() * 16777215 | 0).toString(16);
-    console.log(CurrentColor);
-
-    // Set the background color of the body
     StyleSheetForJS.style.backgroundColor = CurrentColor;
 
-    return CurrentColor;
+    var SliderSpeed = document.getElementById("SpeedSlider");
+    Speed = SliderSpeed.value;
+
+    clearInterval(intervalId);
+    intervalId = setInterval(RandomSliderFunc, Speed);
 }
