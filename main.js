8168178f756b1318d59f17332a53544c3785c15b
DallinVader <dallin@riafox.com>
2023-08-25 10:11:20 -0600

    Added Comments and fixed colorchanger function name

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-function ChangePageToCollorChanger(){
+// Opens a tab to ColorChange site
+function ChangePageToColorChanger(){
     window.open("ColorChanger", "_blank")
 }
 
+// Opens a tab to NameDisplay site
 function ChangePageToNameDisplay(){
     console.log("Change Page");
     window.open("NameDisplay", "_blank")
