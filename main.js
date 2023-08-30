88ca49dc37ce9e21abe7b9f538fb0ce92f4920bd
DallinVader <dallin@riafox.com>
2023-08-30 10:07:26 -0600

    Added week 2 java script

--- main.js
+++ main.js
@@ -8,3 +8,9 @@ function ChangePageToNameDisplay(){
     console.log("Change Page");
     window.open("NameDisplay", "_blank")
 }
+
+// Opens tab to Week 2 java main.html
+function ChangePageToWeek2Java(){
+    console.log("Change Page");
+    window.open("NameDisplay", "_blank")
+}
