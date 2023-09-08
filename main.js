adffc073ad28ca35e908de0cb2a691a6068c06e4
DallinVader <dallin@riafox.com>
2023-09-08 11:39:24 -0600

    Update main.js

--- main.js
+++ main.js
@@ -6,19 +6,19 @@ var classInfoText = "Web Design /Web App Development 2 (TTA)";
 
 // Opens a tab to ColorChange site
 function ChangePageToColorChanger(){
-    window.open("ColorChanger", "_blank")
+    window.open("ColorChanger")
 }
 
 // Opens a tab to NameDisplay site
 function ChangePageToNameDisplay(){
     console.log("Change Page");
-    window.open("NameDisplay", "_blank")
+    window.open("NameDisplay")
 }
 
 // Opens tab to Week 2 java main.html
 function ChangePageToWeek2Java(){
     console.log("Change Page");
-    window.open("Week2JavaScript", "_blank")
+    window.open("Week2JavaScript")
 }
 
 function OnPageLoaded(){
