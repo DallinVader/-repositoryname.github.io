12fd34812130c5e5d72bf85ea5db3bafd1af8bfc
DallinVader <dallin@riafox.com>
2023-10-18 16:14:40 -0700

    game

--- DefenseGame/main.js
+++ DefenseGame/main.js
@@ -1,24 +1,23 @@
-//DragonAnimation
-const DragonAnimations = [
-    { AnimationName: "DragonDeath", Duration: "0.75s", Steps: "steps(13)", Iterations: "1"},
-    { AnimationName: "DragonIdleFly", Duration: "0.5s", Steps: "steps(7)", Iterations: "infinite"},
-    { AnimationName: "DragonAttack", Duration: "0.75s", Steps: "steps(8)", Iterations: "infinite"},
-    { AnimationName: "DragonSleep", Duration: "2s", Steps: "steps(20)", Iterations: "infinite"}
-  ];
+const canvas = getElementById("Canvas1st")
 
-function SetDragonAnim(){
-    let SelectedState = document.getElementById("ChangeDragonAnim").value;
-    if(SelectedState <= DragonAnimations.length && SelectedState >= 0){
-        PlayAnimation(DragonAnimations[SelectedState]);
-    }
-}
+
+
+
+//Dragon Stuff
+let DragonStyleSheet = document.getElementById("Dragon").style;
+
+PlayAnimation(DragonAnimations[1]);
 
 function PlayAnimation(obj){
-    let DragonStyleSheet = document.getElementById("Dragon").style;
-
+    
     DragonStyleSheet.animationName = obj.AnimationName;
     DragonStyleSheet.animationTimingFunction = obj.Steps;
     DragonStyleSheet.animationDuration = obj.Duration;
     DragonStyleSheet.animationIterationCount = obj.Iterations;
 }
-
+const DragonAnimations = [
+    { AnimationName: "DragonDeath", Duration: "0.75s", Steps: "steps(13)", Iterations: "1"},
+    { AnimationName: "DragonIdleFly", Duration: "0.5s", Steps: "steps(7)", Iterations: "infinite"},
+    { AnimationName: "DragonAttack", Duration: "0.75s", Steps: "steps(8)", Iterations: "infinite"},
+    { AnimationName: "DragonSleep", Duration: "2s", Steps: "steps(20)", Iterations: "infinite"}
+];
